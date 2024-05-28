032729c8ac3ece3d5c4fefa552ae0a1a994e89a2
hendri2212 <cowok_cool320@yahoo.co.id>
2024-05-28 12:25:22 +0800

    add js library

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Download Gambar Kalian</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.7.1.min.js"></script>
     <style>
         .frame {
             overflow: hidden;
@@ -38,11 +39,35 @@
         <?php foreach ($images as $image): ?>
             <div class="col-6 text-center">
                 <div class="frame">
-                    <img src="<?= $image; ?>" alt="Image" class="img-thumbnail">
+                    <img src="<?= $image; ?>" alt="Image" class="img-thumbnail" onclick="showImageModal('<?= $image; ?>')">
                 </div>
                 <a href="download.php?file=<?php echo urlencode($image); ?>" class="btn btn-sm btn-info my-2">Download</a>
             </div>
         <?php endforeach; ?>
     </div>
+    <!-- Modal -->
+    <div id="imageModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="imageModalLabel">Image Preview</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <img id="previewImage" src="" alt="Preview" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="js/bootstrap.min.js"></script>
+    <script>
+        function showImageModal(imageUrl) {
+            var previewImage = document.getElementById('previewImage');
+            previewImage.src = imageUrl;
+            $('#imageModal').modal('show');
+        }
+    </script>
 </body>
 </html>
