2ff0093a17abbd7c7282112ad79676cef9c4e67b
hendri2212 <cowok_cool320@yahoo.co.id>
2024-06-09 00:05:26 +0800

    add service worker

--- code/login.php
+++ code/login.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../assets/logo.png" />
+    <link rel="manifest" href="../manifest.json" />
     <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../app.js"></script>
     <title>Register Page</title>
     <style>
         .form-signin {
---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'readImage-cache-v1';
+const urlsToCache = [
+  './',
+  './code/login.php',
+  './assets/logo.png',
+  './css/bootstrap.min.css',
+  './js/bootstrap.min.js',
+  './js/jquery-3.7.1.min.js',
+  'app.js',
+  'manifest.json',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return the response from the cached version
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
